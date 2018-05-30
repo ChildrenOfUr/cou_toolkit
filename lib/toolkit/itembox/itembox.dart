5d71326d701ddd4d65d755678857be2ddd2eb1af
Andy Castille <andy@robiotic.net>
2018-05-29 22:37:44 -0500

    replace all but itembox with vanilla html/css

--- lib/toolkit/itembox/itembox.dart
+++ lib/toolkit/itembox/itembox.dart
@@ -1,56 +1,49 @@
 library itembox;
 
+import 'package:angular/angular.dart';
 import 'package:dnd/dnd.dart';
-import 'package:polymer/polymer.dart';
 
-@CustomTag('ur-itembox')
-class ItemBox extends PolymerElement
-{
-	@published String item = '';
-	@published int quantity = 0;
+@Component(
+	selector: 'ur-itembox',
+	templateUrl: 'itembox.html',
+	styleUrls: const ['itembox.styles']
+)
+class UrItemBox extends Component {
+	String item = '';
+	int quantity = 0;
 
 	Draggable _dragItem;
 	Dropzone _dropBox;
 
-	ItemBox.created() : super.created()
-	{
-		this.attributes.putIfAbsent('item', () => '');
-		initDragDrop();
-	}
-
-	initDragDrop()
-	{
-		_dragItem = new Draggable(this, avatarHandler: new AvatarHandler.clone());
-		_dropBox = new Dropzone(this);
+	UrItemBox(ElementRef host) {
+		_dragItem = new Draggable(host.nativeElement, avatarHandler: new AvatarHandler.clone());
+		_dropBox = new Dropzone(host.nativeElement);
 
 		// Hide the 'panel' element of our dragged object
-		_dragItem.onDrag.listen((_)
-		{
-			_dragItem.avatarHandler.avatar.shadowRoot.querySelector('ur-panel').style
+		_dragItem.onDrag.listen((_) {
+			_dragItem.avatarHandler.avatar
+				.querySelector('.panel')
+				.style
 				..backgroundColor = 'transparent'
 				..border = 'none';
 		});
 
-		_dropBox.onDrop.listen((d)
-		{
+		_dropBox.onDrop.listen((d) {
 			// Don't drag nothingness
-			if (d.draggableElement.item == '')
+			if (d.draggableElement.attributes['item'] == '') {
 				return;
+			}
 
 			// Add to similar item stacks, but don't add to different stacks
-			if (d.dropzoneElement.item != '')
-			{
-				if (d.dropzoneElement.item == d.draggableElement.item)
-				{
-					int total = d.draggableElement.quantity + d.dropzoneElement.quantity;
+			if (d.dropzoneElement.attributes['item'] != '') {
+				if (d.dropzoneElement.attributes['item'] == d.draggableElement.attributes['item']) {
+					int total = int.parse(d.draggableElement.attributes['quantity']) + int.parse(d.dropzoneElement.attributes['quantity']);
 					d.draggableElement.attributes['quantity'] = total.toString();
-				}
-				else
-				{
+				} else {
 					//since this swaps all the attributes of each element
 					//it effectively swaps the elements
-					Map<String,String> temp = {};
-					d.draggableElement.attributes.forEach((key,value) => temp[key] = value);
+					Map<String, String> temp = {};
+					d.draggableElement.attributes.forEach((key, value) => temp[key] = value);
 					d.draggableElement.attributes = d.dropzoneElement.attributes;
 					d.dropzoneElement.attributes = temp;
 					return;
@@ -60,7 +53,7 @@ class ItemBox extends PolymerElement
 			d.dropzoneElement.attributes['item'] = d.draggableElement.attributes['item'];
 			d.dropzoneElement.attributes['quantity'] = d.draggableElement.attributes['quantity'];
 			d.draggableElement.attributes['item'] = '';
-			d.draggableElement.attributes['quantity'] = 0.toString();
+			d.draggableElement.attributes['quantity'] = "0";
 		});
 	}
 }
